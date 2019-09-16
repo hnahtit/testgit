5c4bfddea12066d428a45a08d75e1b9fd8f87896
BuiDuy0408 <54673782+BuiDuy0408@users.noreply.github.com>
2019-09-16 07:32:59 +0700

    Duy

---
+++ Bai_1/quanlynhansu/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace quanlynhansu
+{
+    public partial class f1 : Form
+    {
+        public f1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SqlConnection conn = new SqlConnection(@"Data Source=DESKTOP-CE6UIQC\SQLEXPRESS;Initial Catalog=Bai1_baitapnhom;Integrated Security=True");
+            string sqlSelect = "Select * from BangDangNhap where TaiKhoan = N'" + textBox1.Text + "'and MatKhau = N'" + textBox2.Text+"'";
+            conn.Open();
+            SqlCommand cmd = new SqlCommand(sqlSelect, conn);
+            SqlDataReader reader = cmd.ExecuteReader();
+            if(reader.Read()==true)
+            {
+                this.Hide();
+                Form main = new Main();
+                main.Show();
+            }
+            else
+            {
+                MessageBox.Show("Bạn đăng nhập không thành công");
+                textBox1.Text = "";
+                textBox2.Text = "";
+                textBox1.Focus();
+            }
+            conn.Close();
+        }
+    }
+}
